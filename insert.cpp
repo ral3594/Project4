63bf0940f1657293de03cc42035bead8ca5b0ae5
ral3594 <raless@umich.edu>
2014-11-20 20:14:53 +0000

    Working insert without index# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	modified:   insert.cpp## Changes not staged for commit:#	modified:   indexselect.cpp#	modified:   utility.h## Untracked files:#	.c9/#	dbcreate#	dbcreate.o#	dbdestroy#	dbdestroy.o#	error.o#	heapfile.o#	index.o#	indexselect.o#	inl.o#	insert.o#	join.o#	minirel#	minirel.o#	print.o#	scanselect.o#	select.o#	smj.o#	snl.o#	sort.o#	testdb/#

--- insert.cpp
+++ insert.cpp
@@ -1,7 +1,9 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
-
+#include "utility.h"
+#include <cstdlib>
+#include <cstring>
 /*
  * Inserts a record into the specified relation
  *
@@ -14,7 +16,53 @@ Status Updates::Insert(const string& relation,      // Name of the relation
                        const int attrCnt,           // Number of attributes specified in INSERT statement
                        const attrInfo attrList[])   // Value of attributes specified in INSERT statement
 {
-    /* Your solution goes here */
-
+    Status relstatus;
+    HeapFile relhf(relation, relstatus);
+    if (relstatus != OK){
+        return relstatus;
+    }
+    else{
+        AttrDesc *arrayofattr;
+        int num = attrCnt;
+        attrCat->getRelInfo(relation, num, arrayofattr);
+        int sumsizes = 0;
+        for (int i = 0; i < num; i++){
+            sumsizes = sumsizes + arrayofattr[i].attrLen;
+        }
+        Record rec;
+        rec.length = sumsizes;
+        rec.data = (char *)malloc(sumsizes);
+        //*(char *)rec.data = 'h';
+        //attrdesc tells you where it should be, attrinfo how it was passed to you
+        //for loop - use memcpy (rec.data + offset, attrList[i].attrValue, length);
+        for (int i = 0; i < num; i++){
+           // if (arrayofattr[i].attrName == attrList[i].attrName){
+             //   memcpy((char *)rec.data + arrayofattr[i].attrOffset, attrList[i].attrValue, arrayofattr[i].attrLen);
+            //}
+            //else{
+                for (int j = 0; j < num; j++){
+                    if (strcmp(arrayofattr[i].attrName, attrList[j].attrName) == 0){
+                        memcpy((char *)rec.data + arrayofattr[i].attrOffset, attrList[j].attrValue, arrayofattr[i].attrLen);
+                    }
+                }
+            //}
+        }
+        RID oRID;
+        //*(char *)rec.data = 'h';
+        Status checker = relhf.insertRecord(rec, oRID);
+        if (checker != OK){
+            return relstatus;
+        }
+        //if there is an index on that attribute then i should insert it into that index -- attr desc for loop
+        //insert record first cause need record id..number and type of index 
+        //open
+        //insert on any index
+        //loop through array of attrDesc check if there is an index and then open the file if there is and insert the record
+        //check out index.h
+        Utilities::Print(relation);
+    }
     return OK;
 }
+//char *x = malloc(reclen)
+//free(x);
+//check dynamic vs. status
